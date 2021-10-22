aeea636b82fec9b8bbb585f7754d377b4edc45d3
Kait <kweishaar@hawk.iit.edu>
2021-10-22 08:45:18 -0500

    initial files, next restructure for portfolio cards

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import coverImage from '../../assets/cover/cover-image.jpg';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          I'm a full stack web developer and designer, with a background in Architecture and Engineering. I have a Bachelor's of Architecture and a Master's in Structural Engineering, which have helped hone my design thinking and critical problem solving skills.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/PhotoList/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Modal from '../Modal';
+
+const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
+  const [photos] = useState([
+    {
+      name: 'Volley-All',
+      category: 'full-stack',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Pirate Advice',
+      category: 'full-stack',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Horiseon',
+      category: 'front-end',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    },
+    {
+      name: 'Run-Buddy',
+      category: 'front-end',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    }
+  ]);
+
+  const currentPhotos = photos.filter((photo) => photo.category === category);
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div>
+      {isModalOpen && <Modal onClose={toggleModal} currentPhoto={currentPhoto} />}
+      <div className="flex-row">
+        {currentPhotos.map((image, i) => (
+          <img
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
+            key={image.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoList;
